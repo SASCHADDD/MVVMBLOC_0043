db7c2c97f70cbe0146bb391bb700155427eb9c2d
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 08:56:04 +0700

    membuat bloc untuk mengubah nama

---
+++ lib/viewmodel/user_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:mvvmbloc/viewmodel/user_event.dart';
+import 'package:mvvmbloc/viewmodel/user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  UserBloc() : super(UserState()) {
+    
+    on<NamaChanged>((event, emit) {
+      emit(state.copyWith(nama: event.nama));
+    });
+  }
+}

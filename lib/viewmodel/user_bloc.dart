971363518b1e3fe16939b55046b066e8b7486cfc
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 09:16:41 +0700

    import package

--- lib/viewmodel/user_bloc.dart
+++ lib/viewmodel/user_bloc.dart
@@ -1,14 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mvvmbloc/viewmodel/user_event.dart';
 import 'package:mvvmbloc/viewmodel/user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc() : super(UserState()) {
-
     on<NamaChanged>((event, emit) {
       emit(state.copyWith(nama: event.nama));
     });
 
-    on<EmailChanged>((event, emit){
+    on<EmailChanged>((event, emit) {
       emit(state.copyWith(email: event.email));
     });
   }

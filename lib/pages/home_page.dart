18cb0a95f1f7747014812a2ff1bc893f17f42688
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 09:05:52 +0700

    membuat bar atas dengan mengatur padding dan menyusun di dalamnya

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,5 +4,18 @@ import 'package:mvvmbloc/viewmodel/user_event.dart';
 import 'package:mvvmbloc/viewmodel/user_state.dart';
 
 class HomePage extends StatelessWidget {
-  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("MVVM with BLoC Example")),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
 }

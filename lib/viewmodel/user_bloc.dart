8952aab23458567e89db09e54fc42cc3983834f6
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 08:57:02 +0700

    membua logic mengubah email

--- lib/viewmodel/user_bloc.dart
+++ lib/viewmodel/user_bloc.dart
@@ -3,9 +3,13 @@ import 'package:mvvmbloc/viewmodel/user_state.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc() : super(UserState()) {
-    
+
     on<NamaChanged>((event, emit) {
       emit(state.copyWith(nama: event.nama));
+    });
+
+    on<EmailChanged>((event, emit){
+      emit(state.copyWith(email: event.email));
     });
   }
 }

5f07756dcd396963b2cddaf0026f70be5e3b2b73
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 09:09:51 +0700

    menampilkan data yang di simpan di state

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -25,6 +25,15 @@ class HomePage extends StatelessWidget {
               },
             ),
             SizedBox(height: 20),
+            
+            BlocBuilder<UserBloc, UserState>(
+              builder: (context, state) {
+                return Text(
+                  "Halo, ${state.nama}... , is ${state.email} your electronic mail ?",
+                  style: TextStyle(fontSize: 20),
+                );
+              },
+            ),
           ],
         ),
       ),

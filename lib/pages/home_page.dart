4ca249fc9ad38050035d2658b48bfd1e9f1c15a3
SASCHADDD <saschadanu23@gmail.com>
2026-04-18 09:03:55 +0700

    mmemmbuat tampilna homepage dengan membuat class dengan statelesswidget

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mvvmbloc/viewmodel/user_bloc.dart';
+import 'package:mvvmbloc/viewmodel/user_event.dart';
+import 'package:mvvmbloc/viewmodel/user_state.dart';
+
+class HomePage extends StatelessWidget {
+  
+}
